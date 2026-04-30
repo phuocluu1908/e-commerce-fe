e17e314cc7fed805f7ccbf4aef45410c878f06c8
Blueberry labs <luu@blueberrylabs.com>
2026-04-30 21:43:54 +0700

    update CI

--- cart-mf/eslint.config.mjs
+++ cart-mf/eslint.config.mjs
@@ -1,3 +1,5 @@
+import tsParser from '@typescript-eslint/parser';
+
 export default [
   {
     ignores: ['dist', 'node_modules', '.env*'],
@@ -5,8 +7,14 @@ export default [
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 'latest',
       sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
